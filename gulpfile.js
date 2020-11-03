65d935d85729a41d6137cc15949d25995a601d32
Tumble <tumblegamer@gmail.com>
2020-11-03 01:59:46 +0000

    Building,colors,grid,styling

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+const gulp = require("gulp"),
+	sourcemaps = require("gulp-sourcemaps"),
+	sass = require("gulp-sass"),
+	concat = require("gulp-concat"),
+
+	css = () => {
+		return gulp.src("scss/main.scss")
+			//.pipe(sourcemaps.init())
+			//.pipe(sass({ outputStyle: "compressed" }))
+			.pipe(sass({ outputStyle: "compressed" }))
+			.pipe(concat("bcmc.min.css"))
+			//.pipe(sourcemaps.write({ sourceRoot: "../scss" }))
+			.pipe(gulp.dest("dist"));
+	};
+
+sass.compiler = require('sass');
+
+gulp.task("css", css);
