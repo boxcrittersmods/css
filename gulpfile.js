cb59dd7da433de484fd866e11a3912a2ab474542
Tumble <tumblegamer@gmail.com>
2020-11-07 17:11:31 +0000

    toggle button mechanic added

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,29 @@
 const gulp = require("gulp"),
 	sourcemaps = require("gulp-sourcemaps"),
 	sass = require("gulp-sass"),
-	concat = require("gulp-concat"),
+	rename = require("gulp-rename"),
+	terser = require("gulp-terser"),
 
 	css = () => {
 		return gulp.src("scss/bcmc.scss")
 			.pipe(sourcemaps.init())
-			.pipe(sass())
-			//.pipe(sass({ outputStyle: "compressed" }))
-			.pipe(concat("bcmc.min.css"))
+			//.pipe(sass())
+			.pipe(sass({ outputStyle: "compressed" }))
+			.pipe(rename("bcmc.min.css"))
 			.pipe(sourcemaps.write(".", { sourceRoot: "../scss", includeContent: true }))
+			.pipe(gulp.dest("dist"));
+	},
+	js = () => {
+		return gulp.src("js/bcmc.js")
+			.pipe(sourcemaps.init())
+			.pipe(terser({ warnings: "verbost" }))
+			.pipe(rename("bcmc.min.js"))
+			.pipe(sourcemaps.write(".", { sourceRoot: "../js", includeContent: true }))
 			.pipe(gulp.dest("dist"));
 	};
 
 sass.compiler = require('sass');
 
 gulp.task("css", css);
+gulp.task("js", js);
+gulp.task("build", gulp.series("css", "js"));
--- js/bcmc.js
+++ js/bcmc.js
@@ -1,7 +1,36 @@
-let mobileBtn = document.getElementsByClassName("btn-mobile")[0];
-let nav = document.querySelector("nav");
+(function () {
+	"use strict";
+	const cooldown = 200;
+	let cdEnded = true;
 
-mobileBtn.addEventListener("click", function () {
-	mobileBtn.classList.toggle("open");
-	nav.classList.toggle("open");
-});
+	function toggleEvent() {
+		if (!cdEnded) {
+			console.debug("Toggling is still under cooldown¬");
+			return;
+		}
+		cdEnded = false;
+		setTimeout(() => { console.debug("Toggle cooldown finished."); cdEnded = true; }, cooldown);
+		console.debug("Hello I am", this);
+		let { toggleTarget, toggleClass, toggleSelf } = this.dataset;
+		console.debug({ toggleTarget, toggleClass, toggleSelf });
+		if (void 0 !== toggleSelf) {
+			this.classList.toggle(toggleSelf);
+		}
+		if (void 0 == toggleTarget) return;
+		let target = document.querySelector(toggleTarget);
+		if (void 0 == target) return;
+		console.debug("Target:", target);
+		if (void 0 !== toggleClass) {
+			target.classList.toggle(toggleClass);
+		}
+
+	}
+
+	function setupTogglers() {
+		let togglers = document.querySelectorAll(".toggler");
+		[].forEach.call(togglers, toggler => {
+			toggler.addEventListener("click", toggleEvent);
+		});
+	}
+	setupTogglers();
+})();
